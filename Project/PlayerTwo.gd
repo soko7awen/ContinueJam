1ce77351d4071fb12974263f3e8f627be05cccde
zdicken <zdicken@iu.edu>
2023-05-20 10:02:16 -0400

    changed input mappings to allow wasd and arrow keys
    also removed unnecessary collision from the top floor (player 1 has no gravity)

--- Project/PlayerTwo.gd
+++ Project/PlayerTwo.gd
@@ -10,10 +10,10 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("player2_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("player2_left", "player2_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
