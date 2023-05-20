c95ec8a19dd139dfa04d66f0d53f0e5179442c03
soko Awen <havenhamelin@gmail.com>
2023-05-20 14:57:55 -0400

    Fixed!

--- Project/PlayerOne.gd
+++ Project/PlayerOne.gd
@@ -6,7 +6,7 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	# Add the gravity.
 	#if not is_on_floor():
 	#	velocity.y += gravity * delta
--- Project/main.gd
+++ Project/main.gd
@@ -1,11 +1,9 @@
 extends Node2D
 
 func _ready():
-	var xMin = get_viewport_rect().size.x / 16
-	var xMax = get_viewport_rect().size.x - 20
-
-func _process(delta):
+	#var xMin = get_viewport_rect().size.x / 16
+	#var xMax = get_viewport_rect().size.x - 20
 	pass
 
-func enemySpawn():
-	print("bruh")
+func _process(_delta):
+	pass
