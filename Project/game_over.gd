b51e46ebf5d74a30542e3c6fb4e3718c19ce11de
zdicken <zdicken@iu.edu>
2023-05-21 19:55:38 -0400

    everything is fucked.
    This reverts commit 0c7c520503acc55e0a9ceb7b4e9e5b27c7c2b300.

--- Project/game_over.gd
+++ Project/game_over.gd
@@ -1,9 +1,9 @@
 extends Control
 
-
+var tex
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	if tex:
+		$Image.texture = tex
 func _on_restart_pressed():
 	get_tree().change_scene_to_file("res://main.tscn")
