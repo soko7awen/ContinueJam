c41043be526b2f1c9669d2d771bfa48485cb5cf3
soko Awen <havenhamelin@gmail.com>
2023-05-21 00:15:18 -0400

    Proper collision disabling when falling

--- Project/game_over.gd
+++ Project/game_over.gd
@@ -5,11 +5,5 @@ extends Control
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_restart_pressed():
 	get_tree().change_scene_to_file("res://main.tscn")
