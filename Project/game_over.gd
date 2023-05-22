775056290f64b8d7227d55592af0d20e7744a50a
soko Awen <havenhamelin@gmail.com>
2023-05-21 21:35:32 -0400

    title screen

--- Project/game_over.gd
+++ Project/game_over.gd
@@ -1,9 +1,8 @@
 extends Control
 
-var tex
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if tex:
-		$Image.texture = tex
-func _on_restart_pressed():
-	get_tree().change_scene_to_file("res://main.tscn")
+	pass
+func _input(event):
+	if event.is_action_type() || event is InputEventKey:
+		get_tree().change_scene_to_file("res://main.tscn")
